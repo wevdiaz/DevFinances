2ac7a74559dc8b7b5db0d0846eec0735747b37ea
Weverson Diazz <wevers.info@gmail.com>
2021-06-06 21:37:55 -0300

    tornando os dados dinamicos do front com js

---
+++ scripts.js
@@ -0,0 +1,116 @@
+const transactions = [
+    {
+        id: 1,
+        description: "Luz",
+        amount: -50001,
+        date: "06/06/2021"
+    },
+
+    {
+        id: 2,
+        description: "Website",
+        amount: 500000,
+        date: "06/06/2021"
+    },
+    {
+        id: 3,
+        description: "Água",
+        amount: -20000,
+        date: "06/06/2021"
+    }
+];
+
+const Transaction = {
+
+    all: transactions,
+
+    add(transaction) {
+        Transaction.all.push(transaction);
+    },
+
+    incomes() {
+        let income = 0;
+
+        Transaction.all.forEach( transaction => {
+            if (transaction.amount > 0) {
+                income += transaction.amount;
+            }
+        });
+
+        return income;
+    },
+
+    expenses() {
+        let expense = 0;
+
+        Transaction.all.forEach( transaction => {
+            if (transaction.amount < 0) {
+                expense += transaction.amount;
+            }
+        });
+
+        return expense;
+    },
+
+    total() {
+        
+       return Transaction.incomes() + Transaction.expenses();
+    }
+}
+
+const DOM = {
+    transactionsContainer: document.querySelector("#data-table tbody"),
+
+    addTransaction(transaction, index) {
+
+        const tr = document.createElement("tr");
+        tr.innerHTML = DOM.innerHTMLTransaction(transaction);
+
+        DOM.transactionsContainer.appendChild(tr);
+       
+    },
+
+    innerHTMLTransaction(transaction) {
+        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+        const amount = Utils.formatCurrency(transaction.amount);
+
+        const html = `        
+            <td class="description">${transaction.description}</td>
+            <td class="${CSSclass}">${amount}</td>
+            <td class="date">${transaction.date}</td>
+            <td>
+                <img src="./assets/minus.svg" alt="Remover Transação" onclick="show()">
+            </td>        
+        `
+        return html;
+    },
+
+    updateBalance() {
+        document.getElementById("incomeDisplay").innerHTML = Utils.formatCurrency(Transaction.incomes());
+
+        document.getElementById("expenseDisplay").innerHTML = Utils.formatCurrency(Transaction.expenses());
+
+        document.getElementById("totalDisplay").innerHTML = Utils.formatCurrency(Transaction.total());
+    }
+}
+
+const Utils = {
+    formatCurrency(value) {
+        const signal = Number(value) < 0 ? "-" : "";
+
+        value = String(value).replace(/\D/g, "");
+        value = Number(value) / 100;
+
+        value = value.toLocaleString("pt-BR", {
+            style: "currency",
+            currency: "BRL"
+        });
+
+        return signal + value;
+    }
+}
+
+
+transactions.forEach( transaction => DOM.addTransaction(transaction));
+
+DOM.updateBalance();
